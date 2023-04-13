51fde157e67b97f61f2e453a9010a321237fbb8d
chimingchang <u920392@gmail.com>
2023-04-13 15:04:32 +0800

    Update multitrace.m

--- src/multitrace.m
+++ src/multitrace.m
@@ -59,7 +59,7 @@ Seeds[multiTraceCharge_,nTrace_] := Module[{pivot = Position[multiTraceCharge,_?
 DistriCharges[tmp_] := Module[{lis = {},x},
 	Do[
 		x = {x1,x2,x3,x4,x5};
-		If[Min[Total[x]]>1,
+		If[Min[Total[x]]>minDeg,
 			AppendTo[lis,x];
 		];
 	,
@@ -68,7 +68,7 @@ DistriCharges[tmp_] := Module[{lis = {},x},
 	DeleteDuplicates[Sort/@Transpose/@lis]
 ];
 
-MultiTraceChargeList[charges_] :=Join@@Table[DistriCharges[Seeds[charges,nTrace] ],{nTrace,1,Total[charges]/2}];
+MultiTraceChargeList[charges_] :=Join@@Table[DistriCharges[Seeds[charges,nTrace] ],{nTrace,1,Total[charges]/minDeg}];
 
 MaxDeg[charges_] := Plus@@charges;
 
