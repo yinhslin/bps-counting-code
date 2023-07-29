0886cb54b74141c336d70c852415b044a710e1fc
Ying-Hsuan Lin <yinhslin@gmail.com>
2023-07-29 06:21:39 -0400

    fix local data directory

--- grav_ind/runX.m
+++ grav_ind/runX.m
@@ -54,9 +54,9 @@ home = Switch[user,
 	,
 	_,
 		If[specialQ,
-			Directory[]<>"/bps/"
+			Directory[]<>"/bps/"<>If[schurQ,"grav/","grav16/"]
 			,
-			Directory[]<>"/bps_u/"
+			Directory[]<>"/bps_u/"<>If[schurQ,"grav/","grav16/"]
 		]
 ];
 
